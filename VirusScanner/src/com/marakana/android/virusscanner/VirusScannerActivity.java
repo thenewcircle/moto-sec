bb655a47e945e21c2af9d69185657ed3b2163f55
Marko Gargenta <marko@marakana.com>
2012-06-07 13:00:12 -0700

    Initial checkin

---
+++ VirusScanner/src/com/marakana/android/virusscanner/VirusScannerActivity.java
@@ -0,0 +1,13 @@
+package com.marakana.android.virusscanner;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class VirusScannerActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
